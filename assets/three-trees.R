4dca577ae5b74bd8761359056f9ba80f882a7099
Vincent Clemson <vincent.clemson@perspecta.com>
2020-10-23 20:06:20 -0400

    Use rsvg package for scaling 3 trees diagram

--- assets/three-trees.R
+++ assets/three-trees.R
@@ -1,5 +1,6 @@
 library(DiagrammeR)
 library(DiagrammeRsvg)
+library(rsvg)
 
 g <- create_graph(attr_theme = "tb", graph_name = "three-trees") %>%
   add_node(label = "Working Directory") %>%
@@ -15,11 +16,15 @@ g <- create_graph(attr_theme = "tb", graph_name = "three-trees") %>%
   set_node_attrs(node_attr = "shape", values = "box") %>%
   set_node_attrs(node_attr = "fillcolor", values = "orange") %>%
   set_edge_attrs(edge_attr = "labelfloat", values = "true") %>%
-  set_edge_attrs(edge_attr = "splines", values = "ortho") 
+  set_edge_attrs(edge_attr = "splines", values = "ortho")
 g$nodes_df$style <- "filled,rounded"
 cat(generate_dot(g))
 
 # do manual edits - then, run below
 gv <- grViz(diagram = "./assets/three-trees.gv")
 gv_svg <- export_svg(gv = gv)
-write(x = gv_svg, file = "./assets/three-tress.svg")
+write(x = gv_svg, file = "./assets/img/three-tress.svg")
+rsvg_svg(
+  svg = "./assets/img/three-trees.svg", file = "./assets/img/three-trees-scaled.svg",
+  height = 188*2
+)
