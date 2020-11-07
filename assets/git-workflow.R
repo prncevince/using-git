aa36d3f90ff56263d9a35d24f74913eb6d76efdd
Vincent Clemson <vincent.clemson@perspecta.com>
2020-11-06 21:12:11 -0500

    5 more slides, cover the main git convention/workflow, new workflow graphviz script

--- assets/git-workflow.R
+++ assets/git-workflow.R
@@ -2,16 +2,11 @@ library(DiagrammeR)
 library(DiagrammeRsvg)
 library(rsvg)
 
-g <- create_graph(attr_theme = "tb", graph_name = "three-trees") %>%
-  add_node(label = "Working Directory") %>%
-  add_node(label = "Staging Index") %>%
-  add_node(label = "Commit History / HEAD / Repo") %>%
-  add_edge(
-    from = 1, to = 2, edge_aes = list(label = "git add")
-  ) %>%
-  add_edge(
-    from = 2, to = 3, edge_aes = list(label = "git commit")
-  ) %>%
+g <- create_graph(attr_theme = "lr", graph_name = "three-trees") %>%
+  add_node(label = "Commit \n(or Stash)") %>%
+  add_node(label = "Pull") %>%
+  add_node(label = "Push") %>%
+  add_edges_w_string("1->2 2->3") %>%
   set_node_attrs(node_attr = "fixedsize", values = "false") %>%
   set_node_attrs(node_attr = "shape", values = "box") %>%
   set_node_attrs(node_attr = "fillcolor", values = c("firebrick", "goldenrod", "turquoise")) %>%
@@ -19,13 +14,14 @@ g <- create_graph(attr_theme = "tb", graph_name = "three-trees") %>%
   set_edge_attrs(edge_attr = "labelfloat", values = "true") %>%
   set_edge_attrs(edge_attr = "splines", values = "ortho")
 g$nodes_df$style <- "filled,rounded"
-cat(generate_dot(g))
+writeLines(generate_dot(g), "./assets/git-workflow.gv")
+# cat(generate_dot(g))
 
 # do manual edits e.g. saving to file - then, run below
-gv <- grViz(diagram = "./assets/three-trees.gv")
+gv <- grViz(diagram = "./assets/git-workflow.gv")
 gv_svg <- export_svg(gv = gv)
-write(x = gv_svg, file = "./assets/img/three-tress.svg")
+write(x = gv_svg, file = "./assets/img/git-workflow.svg")
 rsvg_svg(
-  svg = "./assets/img/three-trees.svg", file = "./assets/img/three-trees-scaled.svg",
-  height = 188*2
+  svg = "./assets/img/git-workflow.svg", file = "./assets/img/git-workflow-scaled.svg",
+  height = 44*2
 )
